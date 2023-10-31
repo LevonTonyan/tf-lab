d3b7f460f0fa0f2d1968065960c561c44f9c2d4a
Levon <levontonyan87@gmail.com>
2023-10-31 14:45:32 +0400

    add corretions to tags, task 3 completed

--- base/storage.tf
+++ base/storage.tf
@@ -3,7 +3,6 @@ resource "aws_s3_bucket" "s3_epam" {
   bucket = "epam-tf-lab-${random_string.random.result}"
 
   tags = {
-    Name      = "Levon-Tonyan-01-rt"
     Terraform = true
     Project   = "epam-tf-lab"
     Owner     = "Levon-Tonyan"
@@ -12,7 +11,9 @@ resource "aws_s3_bucket" "s3_epam" {
 
 
 resource "random_string" "random" {
-  length           = 16
-  special          = true
-  override_special = "/@£$"
+  length           = 8
+  override_special = "_ "
+  special = false
+  lower = true
+  upper = false
 }
